ef13051545e84e7f908d3e01d847222ecc8a9794
Torrex1 <flendik666@gmail.com>
2026-03-25 00:52:25 +0300

    init

---
+++ magicStardew/ModEntry.cs
@@ -0,0 +1,30 @@
+﻿using StardewModdingAPI;
+using StardewModdingAPI.Events;
+using StardewValley;
+
+namespace magicStardew
+{
+    public class ModEntry : Mod
+    {
+        public override void Entry(IModHelper helper)
+        {
+            helper.Events.GameLoop.SaveLoaded += this.OnSaveLoaded;
+        }
+
+        private void OnSaveLoaded(object sender, SaveLoadedEventArgs e)
+        {
+            // если предмета нет в инвентаре, то создаем его
+            if (!Game1.player.Items.Any(item => item != null && item.ItemId == "MagicBook"))
+            {
+                Item magicBook = ItemRegistry.Create("MagicBook");
+
+                if (!Game1.player.addItemToInventoryBool(magicBook))
+                {
+                    // если не удалось добавить в инвентарь, то выбрасываем его на землю
+                    Game1.createItemDebris(magicBook, Game1.player.getStandingPosition(), Game1.player.FacingDirection);
+                }
+                this.Monitor.Log("Magic Book added to inventory or dropped on the ground.", LogLevel.Info);
+            }
+        }
+    }
+}
